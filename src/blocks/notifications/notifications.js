dd0af77e9ee83f590e4f796fc7d36ed8ddaa98f2
Zheka-ded <11vitamin11@rambler.ru>
2019-05-15 02:20:55 +0300

    15/05/19(slowly but surely)

---
+++ src/blocks/notifications/notifications.js
@@ -0,0 +1,8 @@
+// var btn1 = document.getElementById(`addNotif`);
+
+// var btnActive = btn1.querySelector(`.notifications-menu__inputSearch-wrap`);
+
+// btn1.onclick = function () {
+//     btn1.classList.toggle(`notifications-menu__inputSearch-wrapActive`);
+// };
+

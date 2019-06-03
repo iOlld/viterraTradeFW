eaf56e38248f5d0d68b8ceff951307481d236ed5
Zheka-ded <11vitamin11@rambler.ru>
2019-06-03 19:57:48 +0300

    03/06/19(test-3 for js)

--- build/js/headerVSRegForm/headerVSRegForm.js
+++ build/js/headerVSRegForm/headerVSRegForm.js
@@ -8,17 +8,17 @@ btnMenu.addEventListener(`click`, function (e) {
 });
 
 
-const activeButton = document.getElementById(`regMenu`);
-const activeUserSvg = document.querySelector(`.headerVSRegForm__svgUser`);
-const activeArrowSvg = document.querySelector(`.headerVSRegForm__svgRegArrow`);
-const activeForm = document.querySelector(`.headerVSRegForm__form-wrap`);
-const zIndexDdm = document.querySelector(`.header__btnForDdm`);
+// const activeButton = document.getElementById(`regMenu`);
+// const activeUserSvg = document.querySelector(`.headerVSRegForm__svgUser`);
+// const activeArrowSvg = document.querySelector(`.headerVSRegForm__svgRegArrow`);
+// const activeForm = document.querySelector(`.headerVSRegForm__form-wrap`);
+// const zIndexDdm = document.querySelector(`.header__btnForDdm`);
 
-activeButton.onclick = function () {
-    this.classList.toggle(`headerVSRegForm__registrationMenuActive`);
-    activeUserSvg.classList.toggle(`headerVSRegForm__svgUserActive`);
-    activeArrowSvg.classList.toggle(`headerVSRegForm__svgRegArrowActive`);
-    activeForm.classList.toggle(`headerVSRegForm__form-wrapActive`);
-    zIndexDdm.classList.toggle(`header__zIndexDdm`);
-};
+// activeButton.onclick = function () {
+//     this.classList.toggle(`headerVSRegForm__registrationMenuActive`);
+//     activeUserSvg.classList.toggle(`headerVSRegForm__svgUserActive`);
+//     activeArrowSvg.classList.toggle(`headerVSRegForm__svgRegArrowActive`);
+//     activeForm.classList.toggle(`headerVSRegForm__form-wrapActive`);
+//     zIndexDdm.classList.toggle(`header__zIndexDdm`);
+// };
 
